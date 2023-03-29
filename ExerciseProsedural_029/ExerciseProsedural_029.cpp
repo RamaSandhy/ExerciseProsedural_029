1a07e7b468926aa531ae7d5683ef0d486baa1b06
RamaSandhy <rshandy07@gmail.com>
2023-03-29 14:57:01 +0700

    prosedural

--- ExerciseProsedural_029/ExerciseProsedural_029.cpp
+++ ExerciseProsedural_029/ExerciseProsedural_029.cpp
@@ -5,3 +5,41 @@ double rerata(double a, double b)
 {
 	return (a + b) / 20;
 }
+
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b)
+{
+	return (a + b) / 20;
+}
+string status(double d)
+{
+	if (d >= 70)
+		return "diterima";
+	else
+		return "diterima";
+}
+string status2(double r, double n)
+{
+	if (r >= 70 && n >= 80)
+		return "ditolak";
+	else
+		return "ditolak";
+}
+int main()
+{
+
+	double nilM, nilB;
+
+	cout << "masukkan nilai matematika B : ";
+	cin >> nilM;
+	cout << "masukkan nilai bahasa inggris A : ";
+	cin >> nilB;
+
+	//rerata = (nilM + nilB) / 20;
+	 //rata = rerata (nilM, nilB);
+	 //st = status (rata);
+	//st = status (rerata (nilM, nilB));
+	cout << "status kelulusannya = " << status(rerata(nilM, nilB)) << endl;
+	cout << "status kelulusannya = " << status2(rerata(nilM, nilB), nilM);
